f05f1118ae11df39ecd571483523f31e5439493c
Ravi Shankar <ravi.shankar@in-it2109.globant.com>
2019-08-23 16:30:49 +0530

    added career

--- src/components/routing/Routing.js
+++ src/components/routing/Routing.js
@@ -3,6 +3,7 @@ import { Route, Link, BrowserRouter as Router } from "react-router-dom";
 import Home from "../home/Home";
 import About from "../about/About";
 import User from "../user/User";
+import Career from "../career/Career";
 
 class Routing extends Component {
   render() {
@@ -17,11 +18,15 @@ class Routing extends Component {
               <li>
                 <Link to="/about">About</Link>
               </li>
+              <li>
+                <Link to="/career">Career</Link>
+              </li>
             </ul>
           </div>
           <Route exact path="/" component={Home} />
           <Route path="/user" component={User} />
           <Route path="/about" component={About} />
+          <Route path="/career" component={Career} />
         </Router>
       </div>
     );
